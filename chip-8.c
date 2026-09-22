20ed8889a41a35e255ffb635eb9b261c4340ae4f
ernesto905 <ernesto@enriquez.dev>
2026-09-22 18:02:25 -0400

    Added main function

--- chip-8.c
+++ chip-8.c
@@ -1,4 +1,28 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[]) 
 { 
+  if (argc != 2) {
+    printf("Your program received %d arguments but expected 1.\n\n", argc - 1); 
+    printf("Example:\n");
+    printf("\t./chip-8 tests/IBM_Logo_test.ch8\n");
+    return 1;
+  }
+  
+  /*TODO && Notes
+   * - [] read its bytes
+   *      to use plain char or to not use plain char, that is the question ; 
+   *      https://stackoverflow.com/questions/13642381/c-c-why-to-use-unsigned-char-for-binary-data
+   * - [] create data structures 
+   * - [] the switch statement 
+   * - [] load it into memory (Remember to start at addr 200)
+   * */
+
+  FILE *ch8_file_ptr;
+  ch8_file_ptr = fopen(argv[1], "r");
+
+  char ch8_program[1024]; // we'll assume the program is 1024 bytes for now
+  fgets(ch8_program, 1024, ch8_file_ptr);
+
   return 0;
 }
