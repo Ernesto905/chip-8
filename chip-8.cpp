cf5040d98ffe9cd7324d39d4122d15b6011ae0e3
ernesto905 <ernesto@enriquez.dev>
2026-09-22 19:05:31 -0400

    Add function to read in the ch8 program into a byte vector

--- chip-8.cpp
+++ chip-8.cpp
@@ -1,16 +1,37 @@
-#include <stdio.h>
+#include <iosfwd>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <fstream>
 
-int main(int argc, char *argv[]) 
+std::vector<std::byte> ReadProgram(char* file_name) 
+{
+  std::ifstream ch8_file(file_name, std::ios::binary);
+  ch8_file.unsetf(std::ios::skipws);
+  std::streampos file_size;
+
+  ch8_file.seekg(0, std::ios::end);
+  file_size = ch8_file.tellg();
+  ch8_file.seekg(0, std::ios::beg);
+
+  std::vector<std::byte> program;
+  program.reserve(file_size);
+  ch8_file.read(reinterpret_cast<char *>(std::data(program)), file_size);
+  
+  return program;
+}
+
+int main(int argc, char* argv[]) 
 { 
   if (argc != 2) {
-    printf("Your program received %d arguments but expected 1.\n\n", argc - 1); 
-    printf("Example:\n");
-    printf("\t./chip-8 tests/IBM_Logo_test.ch8\n");
+    std::cout << "Your program received " << argc - 1 << " arguments but expected 1." << std::endl;
+    std::cout << "Example:" << std::endl;
+    std::cout << "\t./chip-8 tests/IBM_Logo_test.ch8" << std::endl;
     return 1;
   }
   
   /*TODO && Notes
-   * - [] read its bytes
+   * - [x] read its bytes
    *      to use plain char or to not use plain char, that is the question ; 
    *      https://stackoverflow.com/questions/13642381/c-c-why-to-use-unsigned-char-for-binary-data
    * - [] create data structures 
@@ -18,11 +39,7 @@ int main(int argc, char *argv[])
    * - [] load it into memory (Remember to start at addr 200)
    * */
 
-  FILE *ch8_file_ptr;
-  unsigned char ch8_program[1024]; // we'll assume the program is 1024 bytes for now
-  
-  ch8_file_ptr = fopen(argv[1], "r");
-  fgets(ch8_program, 1024, ch8_file_ptr);
+  std::vector<std::byte> program = ReadProgram(argv[1]);
 
   return 0;
 }
